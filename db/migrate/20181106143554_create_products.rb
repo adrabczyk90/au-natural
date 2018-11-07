82075cd5a4c64c537505860a921f1d6322ba66b8
Agnieszka Drabczyk <x14127351@student.ncirl.ie>
2018-11-07 11:25:13 +0000

    adding user and admin

--- db/migrate/20181106143554_create_products.rb
+++ db/migrate/20181106143554_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :title, null: false
+      t.string :title, limit: 150, null: false
+       t.decimal :price, precision: 15, scale: 2, null: false, default: 0
       t.text :description
-      t.decimal :price, precision: 15, scale: 2 , null: false
 
       t.timestamps
     end
