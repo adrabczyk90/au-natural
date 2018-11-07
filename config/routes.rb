63233c17034a4be0c9b680fbe27e8ab2501bdf86
Agnieszka Drabczyk <x14127351@student.ncirl.ie>
2018-11-07 11:42:09 +0000

    adding controllers and models for order items and categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resources :order_items
+  resources :categories
   resources :admins
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'categories#index'
 
-  resources :categories, only: [:index] do
+    resources :categories, only: [:index] do
     resources :products, only: [:index]
   end
 
